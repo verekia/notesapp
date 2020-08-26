716b45266428e259457721c37ec807aca560020b
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-08-26 14:34:08 +0200

    Add events endpoint for Hasura logging

---
+++ src/pages/api/events.ts
@@ -0,0 +1,21 @@
+import logger from 'loglevel'
+
+import { NextApiRequest as Req, NextApiResponse as Res } from 'next'
+
+logger.enableAll()
+
+export default async (req: Req, res: Res) => {
+  if (req.headers['event_handler_hasura_secret'] !== process.env.EVENT_HANDLER_HASURA_SECRET) {
+    return res.status(403).end()
+  }
+
+  const { event, table } = req.body
+
+  if (table.name === 'user') {
+    if(event.op === 'INSERT') {
+      logger.info({ type: 'sign-up', email: event.data.new.email })
+    }
+  }
+
+  res.status(200).end()
+}
