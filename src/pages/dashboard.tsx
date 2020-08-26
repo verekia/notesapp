0da47cc19c99a4679a693612612a9bf3a99b8598
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-08-26 18:50:51 +0200

    Move imports to test git hook

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
+import Box from '@material-ui/core/Box'
 import Container from '@material-ui/core/Container'
 import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Dialog from '@material-ui/core/Dialog'
